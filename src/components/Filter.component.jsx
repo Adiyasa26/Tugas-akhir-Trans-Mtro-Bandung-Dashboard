c441c7e01895fe11cb3bc06b1c7dd95de091c3a5
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-02-09 11:35:53 +0700

    Add some icon and add one filter in dashboard

--- src/components/Filter.component.jsx
+++ src/components/Filter.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import FilterWaktu from './filterWaktu.component';
+
 import './Filter.style.css';
 
 function Filter() {
   return (
     <div className="grid-container">
-      <div className="grid-item item1">1</div>
+      <div className="grid-item item1">
+        <FilterWaktu />
+      </div>
       <div className="grid-item item2">2</div>
       <div className="grid-item item3">3</div>
       <div className="grid-item item4"></div>
