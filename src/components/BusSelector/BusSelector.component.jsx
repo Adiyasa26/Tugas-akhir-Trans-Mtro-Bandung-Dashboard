fb5ec037ee7fb6fb28dd06e7762d11e012a5f5e6
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-04-18 06:31:24 +0700

    Add logic how to select card and add few styling

--- src/components/BusSelector/BusSelector.component.jsx
+++ src/components/BusSelector/BusSelector.component.jsx
@@ -1,83 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CardBus from '../CardBus/CardBus.component';
 
 import './BusSelector.style.css'
 
 function BusSelector() {
-    
-    const busInfo = {
-        buses: [
-          {
-            armada: 1,
-            jurusan: 'Cibiru - Cicaheum',
-            pemberhentian: '09:20 - 09.45',
-            lokasi: 'Jl.Jendral Sudirman'
-          },
-          {
-            armada: 2,
-            jurusan: 'Cicaheum - Cibereum',
-            pemberhentian: '09:10 - 09.20',
-            lokasi: 'Jl.Soekarno Hatta'
-          },
-          {
-            armada: 3,
-            jurusan: 'Cibereum - Cicaheum',
-            pemberhentian: '09:10 - 09.20',
-            lokasi: 'Jl.Soekarno Hatta'
-          },
-          {
-            armada: 4,
-            jurusan: 'Cicaheum - Cibiru',
-            pemberhentian: '09:10 - 09.20',
-            lokasi: 'Jl.Soekarno Hatta'
-          },
-          {
-            armada: 5,
-            jurusan: 'Cicaheum - Leuwi Pandjang',
-            pemberhentian: '09:10 - 09.20',
-            lokasi: 'Jl.Soekarno Hatta'
-          },
-          {
-            armada: 6,
-            jurusan: 'Leuwi Pandjang - Cicaheum',
-            pemberhentian: '09:10 - 09.20',
-            lokasi: 'Jl.Soekarno Hatta'
-          },
-          {
-            armada: 7,
-            jurusan: 'Cicaheum - Dago',
-            pemberhentian: '09:10 - 09.20',
-            lokasi: 'Jl.Soekarno Hatta'
-          },
-          {
-            armada: 8,
-            jurusan: 'Dago - Cicaheum',
-            pemberhentian: '09:10 - 09.20',
-            lokasi: 'Jl.Soekarno Hatta'
-          },
-          {
-            armada: 9,
-            jurusan: 'Cicaheum - Buah Batu',
-            pemberhentian: '09:10 - 09.20',
-            lokasi: 'Jl.Soekarno Hatta'
-          },
-          {
-            armada: 10,
-            jurusan: 'Buah Batu - Cicaheum',
-            pemberhentian: '09:10 - 09.20',
-            lokasi: 'Jl.Soekarno Hatta'
-          },
-        ]
-    };
+  const [selectedBus, setSelectedBus] = useState(1);
+  const [buses, setBuses] = useState([
+    {
+      busNumber: 1,
+      destination: 'Cibiru - Cicaheum',
+      stopTime: '09:20 - 09.45',
+      stopLocation: 'Jl.Jendral Sudirman',
+      isSelected: false
+    },
+    {
+      busNumber: 2,
+      destination: 'Cicaheum - Cibereum',
+      stopTime: '09:10 - 09.20',
+      stopLocation: 'Jl.Soekarno Hatta',
+      isSelected: false
+    },
+    {
+      busNumber: 3,
+      destination: 'Cibereum - Cicaheum',
+      stopTime: '09:10 - 09.20',
+      stopLocation: 'Jl.Soekarno Hatta',
+      isSelected: false
+    },
+    {
+      busNumber: 4,
+      destination: 'Cicaheum - Cibiru',
+      stopTime: '09:10 - 09.20',
+      stopLocation: 'Jl.Soekarno Hatta',
+      isSelected: false
+    },
+    {
+      busNumber: 5,
+      destination: 'Cicaheum - Leuwi Pandjang',
+      stopTime: '09:10 - 09.20',
+      stopLocation: 'Jl.Soekarno Hatta',
+      isSelected: false
+    },
+    {
+      busNumber: 6,
+      destination: 'Leuwi Pandjang - Cicaheum',
+      stopTime: '09:10 - 09.20',
+      stopLocation: 'Jl.Soekarno Hatta',
+      isSelected: false
+    },
+    {
+      busNumber: 7,
+      destination: 'Cicaheum - Dago',
+      stopTime: '09:10 - 09.20',
+      stopLocation: 'Jl.Soekarno Hatta',
+      isSelected: false
+    },
+    {
+      busNumber: 8,
+      destination: 'Dago - Cicaheum',
+      stopTime: '09:10 - 09.20',
+      stopLocation: 'Jl.Soekarno Hatta',
+      isSelected: false
+    },
+    {
+      busNumber: 9,
+      destination: 'Cicaheum - Buah Batu',
+      stopTime: '09:10 - 09.20',
+      stopLocation: 'Jl.Soekarno Hatta',
+      isSelected: false
+    },
+    {
+      busNumber: 10,
+      destination: 'Buah Batu - Cicaheum',
+      stopTime: '09:10 - 09.20',
+      stopLocation: 'Jl.Soekarno Hatta',
+      isSelected: false
+    },
+  ]);
 
-    return (
-        <div className="bus-selector--container">
-            {busInfo.buses.map((bus) => {
-              return <CardBus busInfo={bus}/>    
-            })}
-        </div>
-    );
+  return (
+    <div className="bus-selector--container">
+      {buses.map((bus) => {
+        return <CardBus key={bus.busNumber} eachBus={bus} buses={buses} setBuses={setBuses} onClick={() => {setSelectedBus(bus.busNumber)} }
+          isSelected={bus.busNumber === selectedBus} />
+      })}
+    </div>
+  );
 }
 
 export default BusSelector;
--- src/components/CardBus/CardBus.component.jsx
+++ src/components/CardBus/CardBus.component.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import './CardBus.style.css';
 
@@ -6,30 +6,17 @@ import TimeProgressThin from '../../icon/Time_progress_thin.svg';
 import PinLight from '../../icon/Pin_light_thin.svg';
 
 function CardBus(props) {
-  const [isSelected, setIsSelected] = useState(false)
+  let { eachBus, isSelected, buses, setBuses, onClick} = props;
 
-  const selectedHandler = () => {
-    if (isSelected) {
-      const card = document.querySelector('.card');
-      card.classList.add('selected');
-      return setIsSelected(!isSelected);
-    }
-    else {
-      const card = document.querySelector('.card');
-      card.classList.remove('selected');
-      return setIsSelected(!isSelected);
-    }
-  } 
-  
   return (
-    <div className="card" onClick={selectedHandler}>
+    <div key={eachBus.key} className={`card ${isSelected ? 'selected' : ''}`} onClick={onClick}>
       <div className="bus--left">
-        <h1>{props.busInfo.armada}</h1>
+        <h1>{eachBus.busNumber}</h1>
       </div>
       <div className="bus--right">
         <div className="title">
-          <h1>{`Armada ${props.busInfo.armada}`}</h1>
-          <h2>{props.busInfo.jurusan}</h2>
+          <h1>{`Armada ${eachBus.busNumber}`}</h1>
+          <h2>{eachBus.destination}</h2>
         </div>
 
         <div className="detail">
@@ -39,7 +26,7 @@ function CardBus(props) {
             </div>
             <div className="detail_content">
               <h3>Pemberhentian Terakhir</h3>
-              <h3>{props.busInfo.pemberhentian}</h3>
+              <h3>{eachBus.stopTime}</h3>
             </div>
           </div>
           <div className="detail--item">
@@ -48,7 +35,7 @@ function CardBus(props) {
             </div>
             <div className="detail_content">
               <h3>Lokasi Pemberhentian Terakhir</h3>
-              <h3>{props.busInfo.lokasi}</h3>
+              <h3>{eachBus.stopLocation}</h3>
             </div>
           </div>
         </div>
