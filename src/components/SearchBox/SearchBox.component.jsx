8fa1ec85cf37516f3c44394351c3ed408c3e7661
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-04-22 16:58:36 +0700

    Add some styling and implementation on search box

--- src/components/SearchBox/SearchBox.component.jsx
+++ src/components/SearchBox/SearchBox.component.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 import './SearchBox.style.css'
 
+import SearchIcon from '../../icon/Search.svg'
+
 function SearchBox({ placeholder, handleChange }) {
-    return <input
-    className="search"
-    type="search"
-    placeholder={placeholder}
-    onChange={handleChange}
-  />
+    return (
+      <div className='search--container'>
+        <i><img src={SearchIcon} alt="search icon" /></i>
+        <input className="search" type="search" placeholder={placeholder} onChange={handleChange} />
+      </div>
+      
+    )
 }
 
 export default SearchBox;
