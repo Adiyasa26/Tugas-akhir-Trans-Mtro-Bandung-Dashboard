16949211badda7aef8f259a7b04e56d8d19507df
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-12 01:54:22 +0700

    Add Button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,20 @@
+import './style.scss'
+
+const BUTTON_TYPE_CLASSES = {
+  google: 'google-sign-in',
+  inverted: 'inverted',
+  signout: 'sign-out'
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button
+      className={`button-container ${buttonType ? BUTTON_TYPE_CLASSES[buttonType] : ``}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
