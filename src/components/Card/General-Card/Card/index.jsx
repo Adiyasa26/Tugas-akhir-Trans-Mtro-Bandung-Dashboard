80ffa0641777e5493eab2a1df1ad19fdb9368311
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-19 16:16:52 +0700

    Add card style for general card

--- src/components/Card/General-Card/Card/index.jsx
+++ src/components/Card/General-Card/Card/index.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-import './style.scss'
+import './style.scss';
+
+const HEALTH_CARE_CARD_CLASSES = {
+    cold: 'cold',
+    neutral: 'neutral',
+    warm: 'warm'
+}
 
 function Card(props) {
-    return (
-        <div className='universal-card-container'>
-            <div className="universal-card-container--header">
-                <h3>Penumpang</h3>
-                <p>pada {props.tag}</p>
-            </div>
-            <div className="universal-card-container--main">
-                <h1>{props.amount}</h1>
-                <p>penumpang</p>
-            </div>
-        </div>
-    )
+  const { key, headerTitle, headerTag, mainTitle, mainTag, colorType } = props;
+
+  return (
+    <div key={key} className={`universal-card-container ${colorType ? HEALTH_CARE_CARD_CLASSES[colorType] : ``}`}>
+      <div className="universal-card-container--header">
+        <h3>{headerTitle}</h3>
+        <p>{headerTag}</p>
+      </div>
+      <div className="universal-card-container--main">
+        <h1>{mainTitle}</h1>
+        <p>{mainTag}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
