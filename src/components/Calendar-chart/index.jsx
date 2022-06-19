f60228c1fdb800d0a6294ed52e0fcf1f2c4625da
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-19 16:16:24 +0700

    Change color parameter for calendar chart

--- src/components/Calendar-chart/index.jsx
+++ src/components/Calendar-chart/index.jsx
@@ -6,7 +6,7 @@ const MyResponsiveCalendar = ({ data }) => (
     from="2022-01-01"
     to="2022-12-31"
     emptyColor="#eeeeee"
-    colors={['#e0f2fe', '#71c6ff', '#0369a1', '#003856']}
+    colors={['#87D9FF', '#00B0FF', '#068CC9', '#00496B', '#00496B']}
     margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
     yearSpacing={40}
     monthBorderColor="#ffffff"
@@ -16,10 +16,10 @@ const MyResponsiveCalendar = ({ data }) => (
       {
         anchor: 'bottom-right',
         direction: 'row',
-        translateY: 36,
-        itemCount: 4,
+        translateY: 20,
+        itemCount: 5,
         itemWidth: 20,
-        itemHeight: 36,
+        itemHeight: 20,
         itemsSpacing: 12,
         itemDirection: 'right-to-left',
       },
