7007245dd28f4239968717c919de7e1d54e1c151
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-21 22:27:22 +0700

    add revenue card in information page

---
+++ src/components/Card/General-Card/Card-List/Revenue/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+import CardList from '..';
+import RevenueReportCard from '../../../../Revenue-report-card';
+
+const Revenue = props => {
+  const [passengers, setPassengers] = useState(0);
+  const { state, date } = props;
+  const { selectedBus } = state;
+
+  useEffect(() => {
+    let amountDay;
+    if (selectedBus.data[0].date[2] === date[2]) {
+      amountDay = selectedBus.data[0].passengers;
+      setPassengers(amountDay);
+    } else {
+      setPassengers(0);
+    }
+  }, [date, selectedBus.data]);
+
+  return (
+    <CardList
+      className="card-list--container"
+      title={'Total Pendapatan Berdasarkan Penumpang'}
+      cardListType={"revenue"}
+    >
+      <div className="card-list--container-revenue">
+        <RevenueReportCard
+          key="harian"
+          title={'Harian'}
+          revenue={'Rp2.000.000,00'}
+        />
+        <RevenueReportCard
+          key="bulanan"
+          title={'Bulanan'}
+          revenue={'Rp60.000.000,00'}
+        />
+        <RevenueReportCard
+          key="tahunan"
+          title={'Tahunan'}
+          revenue={'Rp720.000.000,00'}
+        />
+      </div>
+    </CardList>
+  );
+};
+
+export default Revenue;
