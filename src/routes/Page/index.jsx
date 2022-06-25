0bdb991bba240aa0c0b8aa208daa407a1b1af89f
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-25 18:53:50 +0700

    Add Page in routes folder

--- src/routes/Page/index.jsx
+++ src/routes/Page/index.jsx
@@ -1,14 +1,42 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
+import { action } from '../../store';
 
+import Menubar from '../../components/MenuBar';
+import SideBar from '../../components/SideBar';
 import Home from './Home';
-import Prediction from './Prediction';
+
+import './style.scss';
 
 const Page = () => {
+  const dispatch = useDispatch();
+
+  const state = useSelector(state => state);
+
+  const setSearch = payload => dispatch(action.setSearch(payload));
+  const setSelectedBus = payload => dispatch(action.setSelectedBus(payload));
+  const setStartDate = payload => dispatch(action.setStartDate(payload));
+
   return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="prediction" element={<Prediction />} />
-    </Routes>
+    <div>
+      <div className="grid">
+        <Menubar className="menubar" />
+        <SideBar
+          className="sidebar"
+          search={setSearch}
+          state={state}
+          selectedBus={setSelectedBus}
+          startDate={setStartDate}
+        />
+        <Routes>
+          <Route
+            index
+            element={<Home state={state} startDate={setStartDate} />}
+          />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
