4cbdc0e2db821015b4e62ea4658f34ae0b9d2364
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-30 17:38:38 +0700

    add logic for fetching data for passengers feature

--- src/components/Card/General-Card/Card-List/Passengers/index.jsx
+++ src/components/Card/General-Card/Card-List/Passengers/index.jsx
@@ -1,22 +1,97 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import CardList from '..';
 import Card from '../../Card';
+import { getBusInformation, getBusData } from '../../../../../utils/firebase/Firebase.utils';
+import { setBusDocument, setBusData, setPassengersData } from '../../../../../store/action';
 
 const Passenger = props => {
-  const [passengers, setPassengers] = useState(0);
   const { state, date } = props;
-  const { selectedBus } = state;
+  const { busesData } = state;
+  const { selectedBus } = busesData;
+
+  const busDocumentMap = useSelector(state => state.busesData.busDocument);
+  const busDataMap = useSelector(state => state.busesData.busData);
+  const passengersMap = useSelector(state => state.passengersData.passengers)
+  
+  const dispatch = useDispatch();  
 
   useEffect(() => {
-    let amountDay;
-    if (selectedBus.data[0].date[2] === date[2]) {
-      amountDay = selectedBus.data[0].passengers;
-      setPassengers(amountDay);
-    } else {
-      setPassengers(0);
+    const getBusInformationMap = async () => {
+      const busDataInformationMap = await getBusInformation();
+      dispatch(setBusDocument(busDataInformationMap));
     }
-  }, [date, selectedBus.data]);
+    getBusInformationMap();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const getBusDataMap = async () => {
+      const busDataArray = [];
+      for(let data=0; data < busDocumentMap.length; data++) {
+        const busDataMap = await getBusData(busDocumentMap[data][0]);
+        busDataArray.push([busDocumentMap[data][1], busDataMap])
+      }
+
+      busDataArray.map(data => {
+        if (selectedBus.busNumber === data[0].busNumber) {
+          dispatch(setBusData(data[1]));
+        }
+        return true;
+      })
+    }
+    getBusDataMap();
+  }, [busDocumentMap, dispatch, selectedBus.busNumber]);
+
+  useEffect(() => {
+    let passengersToday = 0;
+    let passengersThisMonth = 0; 
+    let passengersThisYear = 0;
+
+    console.log(date[0])
+    console.log(date[1])
+    console.log(date[2])
+    console.log(date[3])
+    for(let count = 0; count < busDataMap.length; count++) {
+      if (
+        date[2] === busDataMap[count].date[2] &&
+        date[1] === busDataMap[count].date[1] &&
+        date[3] === busDataMap[count].date[3]
+        ) {
+        passengersToday = busDataMap[count].mask + busDataMap[count].nomask;
+      } 
+      
+      if (
+        date[1] === busDataMap[count].date[1] &&
+        date[3] === busDataMap[count].date[3]
+        ) {
+        passengersThisMonth = busDataMap[count].mask + busDataMap[count].nomask + passengersThisMonth;
+      }
+
+      if (
+        date[3] === busDataMap[count].date[3]
+        ) {
+        passengersThisYear = busDataMap[count].mask + busDataMap[count].nomask + passengersThisYear;
+      }
+
+      else {
+        passengersToday = 0;
+        passengersThisMonth = 0; 
+        passengersThisYear = 0;
+      }
+    }
+    dispatch(setPassengersData({ today: passengersToday, thismonth: passengersThisMonth, thisyear: passengersThisYear }));
+  }, [busDataMap, date, dispatch])
+
+  // useEffect(() => {
+  //   let amountDay;
+  //   if (setBuses.data[0].date[2] === date[2]) {
+  //     amountDay = setBuses.data[0].passengers;
+  //     setPassengers(amountDay);
+  //   } else {
+  //     setPassengers(0);
+  //   }
+  // }, [date, setBuses.data]);
 
   return (
     <CardList
@@ -28,21 +103,21 @@ const Passenger = props => {
         name={"tanggal"}
         headerTitle={'Penumpang'}
         headerTag={`tanggal ${date[2]}`}
-        mainTitle={passengers}
+        mainTitle={passengersMap.today}
         mainTag={'penumpang'}
       />
       <Card
         name={"bulan"}
         headerTitle={'Penumpang'}
         headerTag={`bulan ${date[1]}`}
-        mainTitle="90000"
+        mainTitle={passengersMap.thismonth}
         mainTag={'penumpang'}
       />
       <Card
         name={"tahun"}
         headerTitle={'Penumpang'}
         headerTag={`tahun ${date[3]}`}
-        mainTitle="108000"
+        mainTitle={passengersMap.thisyear}
         mainTag={'penumpang'}
       />
     </CardList>
