f05b9a8e94c2dc6989045189a108eaa7e63bd22b
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-08-03 01:38:55 +0700

    Add anonymous mode for user

--- src/components/Card/General-Card/Card-List/UserProfile/index.jsx
+++ src/components/Card/General-Card/Card-List/UserProfile/index.jsx
@@ -5,8 +5,8 @@ import CardList from '..';
 const BiodataPenumpang = props => {
   const { state } = props;
   const { busesData, userData } = state;
-  const { selectedBus } = busesData
-  const { currentUser, userRole } = userData
+  const { selectedBus } = busesData;
+  const { currentUser, userRole } = userData;
 
   return (
     <CardList
@@ -17,11 +17,15 @@ const BiodataPenumpang = props => {
       <div className="biodata--container">
         <div className="biodata--container-header">
           <div className="biodata--container-header__left">
-            <h1>{currentUser.displayName[0].toUpperCase()}</h1>
+            <h1>
+              {currentUser ? currentUser.displayName[0].toUpperCase() : 'A'}
+            </h1>
           </div>
           <div className="biodata--container-header__right">
             <div className="biodata--container-header__right-header">
-              {currentUser.displayName.toUpperCase()}
+              {currentUser
+                ? currentUser.displayName.toUpperCase()
+                : 'Anonymous'}
               <h2>{`Anda adalah ${userRole}`}</h2>
             </div>
           </div>
@@ -30,12 +34,8 @@ const BiodataPenumpang = props => {
           {`Tujuan anda kali ini adalah ${selectedBus.destination}`}
         </div>
         <div className="biodata--container-footer">
-          <p>
-            {selectedBus.detailInformation}
-          </p>
-          <p>
-          {selectedBus.additionalInformation}
-          </p>
+          <p>{selectedBus.detailInformation}</p>
+          <p>{selectedBus.additionalInformation}</p>
         </div>
       </div>
     </CardList>
