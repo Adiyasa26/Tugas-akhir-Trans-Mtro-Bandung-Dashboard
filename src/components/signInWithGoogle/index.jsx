a1650f9067a74171c63a4fcd0a3fc5f228a1d2cc
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-08-03 01:36:14 +0700

    Add Sign In Container for User

--- src/components/signInWithGoogle/index.jsx
+++ src/components/signInWithGoogle/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import {
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/Firebase.utils';
 
@@ -12,14 +11,15 @@ import './style.scss';
 
 const SignInWithGoogleContainer = () => {
   const SignInWithGoogle = async () => {
-    const { user } = await signInWithGoogleRedirect();
+    const { user } = await signInWithGooglePopup();
     await createUserDocumentFromAuth(user);
   };
 
   return (
-    <div className="sign-in-page">
-      <div className="sign-in-container">
-        <h2>Dashboard Trans Metro Bandung</h2>
+    <div className="sign-in-page-user">
+      <div className="sign-in-container-user">
+        <h2>Anda Belum Masuk</h2>
+        <p>Pastikan masuk terlebih dahulu sebelum menulis ulasan. Terimakasih.</p>
         <Button type="button" buttonType="google" onClick={SignInWithGoogle}>
           Masuk dengan Google
         </Button>
