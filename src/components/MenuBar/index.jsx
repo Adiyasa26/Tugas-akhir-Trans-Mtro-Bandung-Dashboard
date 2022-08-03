14e13fe5ffff1102fb2b8c174185344f96c77851
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-08-03 17:29:24 +0700

    add back logo in menu bar

--- src/components/MenuBar/index.jsx
+++ src/components/MenuBar/index.jsx
@@ -6,17 +6,20 @@ import { SignOutUser } from '../../utils/firebase/Firebase.utils';
 
 import './style.scss';
 
+import BackLogoActive from '../../icon/Out_1.svg';
 import HomeLogoActive from '../../icon/Home.svg';
 import TimeProgressActive from '../../icon/Time_progress.svg';
+import BackLogo from '../../icon/Out.svg';
 import HomeLogo from '../../icon/Home_gray.svg';
 import TimeProgress from '../../icon/Time_progress_grey.svg';
 import UserLogo from '../../icon/User_box_fill.svg';
 import SignOutLogo from '../../icon/Sign_out_squre_light.svg';
 
 const MenuBar = () => {
+  const [backActive, setBackActive] = useState(false);
   const [homeActive, setHomeActive] = useState(true);
   const [predictionActive, setPredictionActive] = useState(false);
-
+  
   const navigate = useNavigate();
 
   const state = useSelector(state => state);
@@ -37,10 +40,27 @@ const MenuBar = () => {
         <img src={UserLogo} alt="User Logo" />
       </div>
       <div className="menu-container">
+      <Link
+          to="/"
+          onClick={() => {
+            if (backActive === false) {
+              setBackActive(true);
+              setHomeActive(false);
+              setPredictionActive(false);
+            }
+          }}
+          className={backActive ? 'active' : ''}
+        >
+          <img
+            src={backActive === true ? BackLogoActive : BackLogo}
+            alt="Logo Back"
+          />
+        </Link>
         <Link
           to="/trans-metro-bandung/home"
           onClick={() => {
             if (homeActive === false) {
+              setBackActive(false);
               setHomeActive(true);
               setPredictionActive(false);
             }
@@ -56,6 +76,7 @@ const MenuBar = () => {
           to="/trans-metro-bandung/prediction"
           onClick={() => {
             if (predictionActive === false) {
+              setBackActive(false);
               setHomeActive(false);
               setPredictionActive(true);
             }
